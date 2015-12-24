71f545760777c385ae3d74265980567bfd6b18d2
Shaun Burdick <github@shaunburdick.com>
2015-12-24 15:38:35 -0500

    Fixed some linting errors

--- config.default.js
+++ config.default.js
@@ -1,23 +1,24 @@
-var config = {
-    jira: {
-        protocol: 'https',
-        host: 'jira.yourhost.domain',
-        port: 443,
-        base: '',
-        user: 'username',
-        pass: 'password',
-        apiVersion: 'latest',
-        verbose: false,
-        strictSSL: false,
-        regex: /([A-Z]{1}[A-Z0-9]+\-[0-9]+)/g,
-        sprintField: '',
-        customFields: {}
-    },
-    slack: {
-        token: 'xoxb-Your-Token',
-        autoReconnect: true,
-        autoMark: true // Mark messages as read
-    },
-    usermap: {}
+'use strict';
+
+const config = {
+  jira: {
+    protocol: 'https',
+    host: 'jira.yourhost.domain',
+    port: 443,
+    base: '',
+    user: 'username',
+    pass: 'password',
+    apiVersion: 'latest',
+    verbose: false,
+    strictSSL: false,
+    regex: /([A-Z]{1}[A-Z0-9]+\-[0-9]+)/g,
+    sprintField: '',
+    customFields: {},
+  },
+  slack: {
+    token: 'xoxb-Your-Token',
+    autoReconnect: true,
+  },
+  usermap: {},
 };
 module.exports = config;
